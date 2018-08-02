75407dc3254c39b7c51ce8aa76d735d83142d90e
irfanmumtaz <im.irfanmumtaz21@gmail.com>
2018-08-03 02:01:12 +0500

    PyMongo Delete Query

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 from flask_pymongo import PyMongo
-import sys
+import sys, time
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME']='todo'
 app.config['MONGO_URI']='mongodb://todo_task:todo_task*123*@ds111082.mlab.com:11082/todo'
-# app.config['MONGO_DBNAME'] = 'miti'
-# app.config['MONGO_URI'] = 'mongodb://qasim:qasim@ds143030.mlab.com:43030/miti'
+
 mongo = PyMongo(app)
 
 
 
 @app.route('/')
 def index():
-    _tasks = mongo.db.tasks.find()
+    _tasks = mongo.db.tasks.find().sort({'created_at': mongo.DESCENDING})
     return  render_template('index.html', tasks=_tasks)
 
 @app.route('/add_task', methods=['POST'])
 def add_task():
     if request.method == 'POST':
-        data = {'task': request.form['task']}
+        data = {'task': request.form['task'], 'created_at': time.strftime('%d-%m-%Y %H:%M:%S'), 'updated_at': time.strftime('%d-%m-%Y %H:%M:%S')}
         mongo.db.tasks.insert(data)
         return redirect(url_for('index'))
 
@@ -27,8 +27,8 @@ def add_task():
 def task_destroy():
     if request.method == 'GET':
         id = request.args.get('id')
-        mongo.db.tasks.remove({'id': { '$oid': id }})
-        return ''
+        mongo.db.tasks.delete_one({'_id': ObjectId(id)})
+        return redirect(url_for('index'))
 
 
 app.run(debug=True)
