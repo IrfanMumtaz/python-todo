283d823a1789c2a1fb0da97bd6753dab2221c263
irfanmumtaz <im.irfanmumtaz21@gmail.com>
2018-08-03 15:41:09 +0500

    crud operations done

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
-from flask_pymongo import PyMongo
+from flask_cors import CORS
+from flask_pymongo import PyMongo, pymongo
 import sys, time
 from bson.objectid import ObjectId
 
@@ -9,26 +10,75 @@ app.config['MONGO_URI']='mongodb://todo_task:todo_task*123*@ds111082.mlab.com:11
 
 mongo = PyMongo(app)
 
+cors = CORS(app, resources={r'/ajax/*': {"origins": '*'}})
+
 
 
 @app.route('/')
 def index():
-    _tasks = mongo.db.tasks.find().sort({'created_at': mongo.DESCENDING})
+    _tasks = mongo.db.tasks.find().sort('created_at', pymongo.DESCENDING)
     return  render_template('index.html', tasks=_tasks)
 
 @app.route('/add_task', methods=['POST'])
 def add_task():
     if request.method == 'POST':
-        data = {'task': request.form['task'], 'created_at': time.strftime('%d-%m-%Y %H:%M:%S'), 'updated_at': time.strftime('%d-%m-%Y %H:%M:%S')}
-        mongo.db.tasks.insert(data)
+        tasks = mongo.db.tasks
+        data = {
+            'task': request.form['task'],
+            'status': 'view', 
+            'created_at': time.strftime('%d-%m-%Y %H:%M:%S'), 
+            'updated_at': time.strftime('%d-%m-%Y %H:%M:%S')
+            }
+        tasks.insert(data)
         return redirect(url_for('index'))
 
 @app.route('/destroy_task')
 def task_destroy():
     if request.method == 'GET':
         id = request.args.get('id')
-        mongo.db.tasks.delete_one({'_id': ObjectId(id)})
+        tasks = mongo.db.tasks
+        result = tasks.find_one({'_id': ObjectId(id)})
+        tasks.remove(result)
         return redirect(url_for('index'))
+
+
+@app.route('/ajax/task_update', methods=['POST'])
+def task_update():
+    id = request.form['id']
+    tasks = mongo.db.tasks
+    result = tasks.find_one({'_id': ObjectId(id)})
+
+    if result['status'] == 'completed':
+        result['status'] = 'view'
+        res = {"status": 'view'}
+    else:
+        result['status'] = 'completed'
+        res = {"status": 'completed'}
+
+    result['updated_at'] = time.strftime('%d-%m-%Y %H:%M:%S')
+
+    tasks.save(result)
+
+    return jsonify({'status': res});
+
+@app.route('/actives')
+def actives():
+    tasks = mongo.db.tasks
+    _tasks = tasks.find({'status': 'view'}).sort('created_at', pymongo.DESCENDING)
+    return  render_template('index.html', tasks=_tasks)
+
+@app.route('/completes')
+def completes():
+    tasks = mongo.db.tasks
+    _tasks = tasks.find({'status': 'completed'}).sort('created_at', pymongo.DESCENDING)
+    return  render_template('index.html', tasks=_tasks)
+
+@app.route('/clear_completes')
+def clear_completes():
+    tasks = mongo.db.tasks
+    tasks.remove({'status': 'completed'})
+    return redirect(url_for('index'))
+
 
 
 app.run(debug=True)
