ab9d1fd4a28d3cf850b8c2af9d900901b3be2a8b
irfanmumtaz <im.irfanmumtaz21@gmail.com>
2018-08-03 12:46:28 +0500

    complete CRUD

--- static/js/app.js
+++ static/js/app.js
@@ -3,4 +3,27 @@
 
 	// Your starting point. Enjoy the ride!
 
+	$('.todo-list').on('click','.task_update', function(){
+		console.log($(this).val());
+		$(this).parents('li').addClass('temp');
+		$.ajax({
+			'type': 'POST',
+			'url': '/ajax/task_update',
+			'data': {id: $(this).val()},
+			success: function(res){
+				if(res.status.status == 'view'){
+					$('.temp').removeClass("completed temp").addClass('view')
+				}
+				if(res.status.status == 'completed'){
+					$('.temp').removeClass("view temp").addClass('completed')
+				}
+					
+				console.log(res.status.status)
+			},
+			error: function(err){
+				console.log(err)
+			}
+		});
+	});
+
 })(window);
